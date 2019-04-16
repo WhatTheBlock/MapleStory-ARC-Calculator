751d47795e95aa48e310dc89fde86aa57da59800
WhatTheBlock <34786954+WhatTheBlock@users.noreply.github.com>
2019-04-16 19:57:44 +0800

    v1.0

--- mainui.h
+++ mainui.h
@@ -52,6 +52,29 @@ private slots:
 
     void on_ArcDamage_y_textChanged(const QString &arg1);
 
+    void on_d200_textChanged(const QString &arg1);
+
+    void on_d210_textChanged(const QString &arg1);
+
+    void on_d220_textChanged(const QString &arg1);
+
+    void on_d220_vip_textChanged(const QString &arg1);
+
+    void on_d225_vip_textChanged(const QString &arg1);
+
+    void on_d225_textChanged(const QString &arg1);
+
+    void on_d230_textChanged(const QString &arg1);
+
+    void on_d235_textChanged(const QString &arg1);
+
+    void on_targetArc_textChanged(const QString &arg1);
+
+    void on_startDate_userDateChanged(const QDate &date);
+
+
+    void on_vipSwitch_stateChanged(int arg1);
+
 private:
     Ui::MainUI *ui;
 
@@ -62,12 +85,15 @@ private:
     QLabel* ApTotal;
     QComboBox* ArcMode;
     int ArcLVint[6], ArcCurrentint[6], ArcUpgradeint[6];
+    int upgradeList[99];
+    int day;
 
     void upgradeVal();
     void updateAp(int mode);
     void avoidError();
     int upgradeMeso(int from, int to);
     void ArcDamage(int x, int y);
+    void dailyTask();
 
 };
 
