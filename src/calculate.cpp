0cab1f87583e156c75ca7fb7f69b5096519de955
WhatTheBlock <34786954+WhatTheBlock@users.noreply.github.com>
2023-06-19 19:15:30 +0800

    更新計算公式

--- src/calculate.cpp
+++ src/calculate.cpp
@@ -2,34 +2,10 @@
 
 //更新各ARC的升級資訊
 void MainUI::updateArcToolTips(QLabel* arcimg, int arc) {
+    //更新變數
     dailyTask();
 
-    int dailyGet[ARCTYPE] = { 0 };
-    dailyGet[0] = ui->d200->value();
-    dailyGet[1] = ui->d210->value();
-    dailyGet[2] = ui->d220->value();
-    dailyGet[3] = ui->d225->value();
-    dailyGet[4] = ui->d230->value();
-    dailyGet[5] = ui->d235->value();
-
-    bool mobbingMissionState[ARCTYPE] = { 0 };
-    mobbingMissionState[0] = ui->mobbingMission_200->isChecked();
-    mobbingMissionState[1] = ui->mobbingMission_210->isChecked();
-    mobbingMissionState[2] = ui->mobbingMission_220->isChecked();
-    mobbingMissionState[3] = ui->mobbingMission_225->isChecked();
-    mobbingMissionState[4] = ui->mobbingMission_230->isChecked();
-    mobbingMissionState[5] = ui->mobbingMission_235->isChecked();
-
-    int mobbingMission[ARCTYPE] = { 0 };
-    mobbingMission[0] = D200_MOB;
-    mobbingMission[1] = D210_MOB;
-    mobbingMission[2] = D220_MOB;
-    mobbingMission[3] = D225_MOB;
-    mobbingMission[4] = D230_MOB;
-    mobbingMission[5] = D235_MOB;
-
     int arcLv = ArcLV[arc]->value();
-    int characterLV = ui->characterLV->value();
     int arcVal = ArcCurrent[arc]->value();
     int arcVal_max = arcUpgradeList[arcLv - 1] + arcVal;
     int target = ArcUpgrade[arc]->text().toInt();
@@ -43,41 +19,23 @@ void MainUI::updateArcToolTips(QLabel* arcimg, int arc) {
     arcUpgradeDays[arc] = 0;
     arcMaxDays[arc] = 0;
 
-    if(arcLv == 0 || (dailyGet[arc] == 0 && !mobbingMissionState[arc])) {
+    if(arcLv == 0 || (!dailyIsChecked[arc] && !weeklyIsChecked[arc])) {
         arcimg->setToolTip(QStringLiteral("升級所需楓幣：?\n升級所需天數：?\n升級日期：?\n-\n滿級所需楓幣：?\n滿級所需天數：?\n滿級日期：?"));
     }
     else {
-        if(arc == 2) {
-            arcVal *= ARC_TO_COIN_220;
-            arcVal_max *= ARC_TO_COIN_220;
-            target *=  ARC_TO_COIN_220;
-            target_max *=  ARC_TO_COIN_220;
-        }
-        else if(arc == 3) {
-            arcVal *= ARC_TO_COIN_225;
-            arcVal_max *= ARC_TO_COIN_225;
-            target *= ARC_TO_COIN_225;
-            target_max *=  ARC_TO_COIN_225;
-        }
-
-        while(arcVal < target) {
-            if(mobbingMissionState[arc]) {
-                arcVal += mobbingMission[arc];
-                if(arc == 0 && characterLV >= 205) arcVal += D205_MOB;
-                else if(arc == 1 && characterLV >= 215) arcVal += D215_MOB;
+        while (true) {
+            if (dailyIsChecked[arc]) {
+                arcVal += dailyMission[arc];
+                arcVal_max += dailyMission[arc];
             }
-            arcVal += dailyGet[arc];
-            arcUpgradeDays[arc]++;
-        }
-
-        while(arcVal_max < target_max) {
-            if(mobbingMissionState[arc]) {
-                arcVal_max += mobbingMission[arc];
-                if(arc == 0 && characterLV >= 205) arcVal_max += D205_MOB;
-                else if(arc == 1 && characterLV >= 215) arcVal_max += D215_MOB;
+            if (weeklyIsChecked[arc] && (arcMaxDays[arc] % 7 == 0)) {
+                arcVal += weeklyMission[arc];
+                arcVal_max += weeklyMission[arc];
             }
-            arcVal_max += dailyGet[arc];
-            arcMaxDays[arc]++;
+
+            if (arcVal >= target && arcVal_max >= target_max) break;
+            if (arcVal < target) arcUpgradeDays[arc]++;
+            if (arcVal_max < target_max) arcMaxDays[arc]++;
         }
 
         arcimg->setToolTip(QStringLiteral("升級所需楓幣：%1\n升級所需天數：%2\n升級日期：%3\n-\n滿級所需楓幣：%4\n滿級所需天數：%5\n滿級日期：%6")
@@ -93,17 +51,8 @@ void MainUI::updateArcToolTips(QLabel* arcimg, int arc) {
 
 //更新各AUT的升級資訊
 void MainUI::updateAutToolTips(QLabel* autimg, int aut) {
+    //更新變數
     dailyTask_aut();
-
-    bool mobbingMissionState[AUTTYPE] = { 0 };
-    mobbingMissionState[0] = ui->mobbingMission_260->isChecked();
-    mobbingMissionState[1] = ui->mobbingMission_270->isChecked();
-    mobbingMissionState[2] = ui->mobbingMission_275->isChecked();
-
-    int mobbingMission[AUTTYPE] = { 0 };
-    mobbingMission[0] = D260_MOB;
-    mobbingMission[1] = D270_MOB;
-    mobbingMission[2] = D275_MOB;
 
     int autLv = AutLV[aut]->value();
     int autVal = AutCurrent[aut]->value();
@@ -118,19 +67,19 @@ void MainUI::updateAutToolTips(QLabel* autimg, int aut) {
     autUpgradeDays[aut] = 0;
     autMaxDays[aut] = 0;
 
-    if(autLv == 0 || !mobbingMissionState[aut]) {
+    if(autLv == 0 || !dailyIsChecked_aut[aut]) {
         autimg->setToolTip(QStringLiteral("升級所需楓幣：?\n升級所需天數：?\n升級日期：?\n-\n滿級所需楓幣：?\n滿級所需天數：?\n滿級日期：?"));
     }
     else {
-        while(autVal < target) {
-            autVal += mobbingMission[aut];
-            if(aut == 0 && ui->mob265->isChecked()) autVal += D265_MOB;
-            autUpgradeDays[aut]++;
-        }
+        while (autVal < target || autVal_max < AUTMAXTOTAL) {
+            autVal += dailyMission_aut[aut];
+            autVal_max += dailyMission_aut[aut];
+            if (aut == 0 && ui->mob265->isChecked()) {
+                autVal += D265_MOB;
+                autVal_max += D265_MOB;
+            }
 
-        while(autVal_max < AUTMAXTOTAL) {
-            autVal_max += mobbingMission[aut];
-            if(aut == 0 && ui->mob265->isChecked()) autVal_max += D265_MOB;
+            if (autVal < target) autUpgradeDays[aut]++;
             autMaxDays[aut]++;
         }
 
@@ -151,6 +100,19 @@ void MainUI::dailyTask() {
     int maxReachArc = hyperStats + guildSkill; //計算前先加上極限屬性 & 公會技能增加的ARC
     day = 0;
 
+    dailyIsChecked[0] = ui->daily200->isChecked();
+    dailyIsChecked[1] = ui->daily210->isChecked();
+    dailyIsChecked[2] = ui->daily220->isChecked();
+    dailyIsChecked[3] = ui->daily225->isChecked();
+    dailyIsChecked[4] = ui->daily230->isChecked();
+    dailyIsChecked[5] = ui->daily235->isChecked();
+    weeklyIsChecked[0] = ui->weekly200->isChecked();
+    weeklyIsChecked[1] = ui->weekly210->isChecked();
+    weeklyIsChecked[2] = ui->weekly220->isChecked();
+    weeklyIsChecked[3] = ui->weekly225->isChecked();
+    weeklyIsChecked[4] = ui->weekly230->isChecked();
+    weeklyIsChecked[5] = ui->weekly235->isChecked();
+
     //計算前先偵錯
     avoidError();
 
@@ -161,21 +123,13 @@ void MainUI::dailyTask() {
     }
     //若未達到
     else {
-        if(ui->characterLV->value() >= 205)
-            dailyGet[0] = (ui->mobbingMission_200->isChecked()) ? ui->d200->value() + D200_MOB + D205_MOB : ui->d200->value();
-        else dailyGet[0] = (ui->mobbingMission_200->isChecked()) ? ui->d200->value() + D200_MOB : ui->d200->value();
-        if(ui->characterLV->value() >= 215)
-            dailyGet[1] = (ui->mobbingMission_210->isChecked()) ? ui->d210->value() + D210_MOB + D215_MOB : ui->d210->value();
-        else dailyGet[1] = (ui->mobbingMission_210->isChecked()) ? ui->d210->value() + D210_MOB : ui->d210->value();
-        dailyGet[2] = (ui->mobbingMission_220->isChecked()) ? ui->d220->value() + D220_MOB : ui->d220->value();
-        dailyGet[3] = (ui->mobbingMission_225->isChecked()) ? ui->d225->value() + D225_MOB : ui->d225->value();
-        dailyGet[4] = (ui->mobbingMission_230->isChecked()) ? ui->d230->value() + D230_MOB : ui->d230->value();
-        dailyGet[5] = (ui->mobbingMission_235->isChecked()) ? ui->d235->value() + D235_MOB : ui->d235->value();
-
         //計算可能達到的最高ARC
         for(int i = 0; i < ARCTYPE; i++){
             arcLV = ArcLV[i]->value();
-            if(dailyGet[i] != 0) maxReachArc += 220;
+            dailyGet[i] = (dailyIsChecked[i]) ? dailyMission[i] : 0;
+            weeklyGet[i] = (weeklyIsChecked[i]) ? weeklyMission[i] : 0;
+
+            if(dailyGet[i] + weeklyGet[i] != 0) maxReachArc += 220;
             else if(arcLV != 0) maxReachArc += arcLV * 10 + 20;
         }
         //-------------------------------------------------------------
@@ -197,39 +151,29 @@ void MainUI::dailyTask() {
                 cntLv[i] = 0;
                 arcLV = ArcLV[i]->value();
                 arcCurrent = ArcCurrent[i]->value();
-
-                if(arcLV == 0) current[i] = 0;
-                else {
-                    current[i] = arcUpgradeList[arcLV - 1] + arcCurrent;
-                    if(i == 2) current[i] *= ARC_TO_COIN_220; //拉契爾恩轉換為硬幣來計算
-                    else if (i == 3) current[i] *= ARC_TO_COIN_225; //阿爾卡娜轉換為硬幣來計算
-                }
+                current[i] = (arcLV == 0) ? 0 : (arcUpgradeList[arcLV - 1] + arcCurrent);
             }
 
             //迴圈一次等於過一天
             while(count < targetArc) {
                 for(int i = 0; i < ARCTYPE; i++){
                     arcLV = ArcLV[i]->value();
-                    //當前等級+暫存等級小於上限且一天可獲得數量大於0
-                    if((arcLV + cntLv[i]) < ARCMAXLV && dailyGet[i] > 0){
+                    //當前等級+暫存等級小於上限且可獲得數量大於0
+                    if((arcLV + cntLv[i]) < ARCMAXLV && (dailyGet[i] + weeklyGet[i]) > 0){
                         current[i] += dailyGet[i];
+                        if(day % 7 == 0) current[i] += weeklyGet[i];
 
-                        //設定ARC轉硬幣倍率
-                        int a2c;
-                        switch (i) {
-                            case 2: a2c = ARC_TO_COIN_220; break;
-                            case 3: a2c = ARC_TO_COIN_225; break;
-                            default: a2c = 1; break;
-                        }
-
-                        if(current[i] >= (arcUpgradeList[arcLV + cntLv[i]] * a2c)) {
-                            (arcLV + cntLv[i] == 0) ? count += 30 : count += 10;
+                        if (current[i] >= arcUpgradeList[arcLV + cntLv[i]]) {
+                            count += (arcLV + cntLv[i] == 0) ? 30 : 10;
                             cntLv[i]++;
                         }
                     }
                 }
-                day++;
+
+                //起始日獲得量也計算進去，若第一次獲得就達標，所需天數就是0天
+                if(count < targetArc) day++;
             }
+
             ui->targetDays->setNum(day);
             ui->targetDate->setDate(ui->startDate->date().addDays(day));
         }
@@ -242,6 +186,10 @@ void MainUI::dailyTask_aut() {
     int maxReachAut = 0;
     day = 0;
 
+    dailyIsChecked_aut[0] = ui->daily260->isChecked();
+    dailyIsChecked_aut[1] = ui->daily270->isChecked();
+    dailyIsChecked_aut[2] = ui->daily275->isChecked();
+
     //計算前先偵錯
     avoidError();
 
@@ -252,11 +200,10 @@ void MainUI::dailyTask_aut() {
     }
     //若未達到
     else {
-        if(ui->mobbingMission_260->isChecked())
-            dailyGet[0] = (ui->mob265->isChecked()) ? D260_MOB + D265_MOB : D260_MOB;
-        else dailyGet[0] = 0;
-        dailyGet[1] = (ui->mobbingMission_270->isChecked()) ? D270_MOB : 0;
-        dailyGet[2] = (ui->mobbingMission_275->isChecked()) ? D275_MOB : 0;
+        dailyGet[0] = (dailyIsChecked_aut[0] && ui->mob265->isChecked()) ? dailyMission_aut[0] + D265_MOB : dailyMission_aut[0];
+        dailyGet[0] = (dailyIsChecked_aut[0]) ? dailyGet[0] : 0;
+        dailyGet[1] = (dailyIsChecked_aut[1]) ? dailyMission_aut[1] : 0;
+        dailyGet[2] = (dailyIsChecked_aut[2]) ? dailyMission_aut[2] : 0;
 
         //計算可能達到的最高AUT
         for(int i = 0; i < AUTTYPE; i++){
@@ -302,8 +249,11 @@ void MainUI::dailyTask_aut() {
                         }
                     }
                 }
-                day++;
+
+                //起始日獲得量也計算進去，若第一次獲得就達標，所需天數就是0天
+                if(count < targetAut) day++;
             }
+
             ui->targetDays_aut->setNum(day);
             ui->targetDate_aut->setDate(ui->startDate_aut->date().addDays(day));
         }
